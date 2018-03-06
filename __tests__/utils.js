b64cc574198142a5b1a6a0a46ff66926e4546c8f
Michal Matyas <github@higher.lv>
2018-03-06 21:27:25 +0100

    Add tests or makePromise.reject and makePromise.resolve

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -1,17 +1,41 @@
 import {makePromise} from '../src/utils'
 
 describe('makePromise', () => {
-  it('returns a promise with accessible resolve function', () => {
-    const promise = makePromise()
+  describe('resolve', () => {
+    it('returns a promise with accessible resolve function', () => {
+      const promise = makePromise()
 
-    expect(promise).toBeInstanceOf(Promise)
-    expect(promise.resolve).toBeInstanceOf(Function)
+      expect(promise).toBeInstanceOf(Promise)
+      expect(promise.resolve).toBeInstanceOf(Function)
+    })
+
+    it('resolves a promise', done => {
+      const promise = makePromise()
+      promise.then(value => {
+        expect(value).toEqual(123)
+        done()
+      })
+
+      promise.resolve(123)
+    })
   })
 
-  it('returns a promise with accessible reject function', () => {
-    const promise = makePromise()
+  describe('reject', () => {
+    it('returns a promise with accessible reject function', () => {
+      const promise = makePromise()
 
-    expect(promise).toBeInstanceOf(Promise)
-    expect(promise.reject).toBeInstanceOf(Function)
+      expect(promise).toBeInstanceOf(Promise)
+      expect(promise.reject).toBeInstanceOf(Function)
+    })
+
+    it('rejects a promise', done => {
+      const promise = makePromise()
+      promise.then(null, value => {
+        expect(value).toEqual(123)
+        done()
+      })
+
+      promise.reject(123)
+    })
   })
 })
