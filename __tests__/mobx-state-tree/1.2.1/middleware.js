475a66cda3a241b2cbe4a516a778c29257ba0ed7
rabsztok <rabsztok@gmail.com>
2018-03-21 14:11:32 +0100

    Support MST 1.4 and 2.0 versions

---
+++ __tests__/mobx-state-tree/1.2.1/middleware.js
@@ -0,0 +1,45 @@
+import sinon from "sinon"
+import createMiddleware from "../../../src/middleware"
+import track from "../../../src/track"
+import { addMiddleware, flow, types } from "./node_modules/mobx-state-tree"
+
+const trueFn = () => true
+
+let sandbox = sinon.createSandbox()
+
+describe("middleware.js", () => {
+  afterEach(() => sandbox.restore())
+
+  describe("on MST 1.2.1", () => {
+    describe("when renderLimit is reached", () => {
+      it("fails with `false` value", async () => {
+        const delay = sandbox.stub().resolves(true)
+        const Store = track(
+          types.model("Store").actions(() => ({
+            exampleFlow: flow(function*() {
+              yield delay()
+              return 2
+            })
+          })),
+          { exampleFlow: track.async() }
+        )
+
+        const store = Store.create()
+
+        const tracker = {
+          add: sandbox.spy(),
+          has: trueFn,
+          remove: sandbox.spy(),
+          renderLimitReached: trueFn
+        }
+
+        const middleware = createMiddleware(tracker, console)
+
+        addMiddleware(store, middleware)
+
+        const foo = await store.exampleFlow()
+        expect(foo).toBe(null)
+      })
+    })
+  })
+})
